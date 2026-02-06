cec13141cf6b5f42e3e7a2d830ecce0d12840bdd
moritz-grimm <molumo@icloud.com>
2026-02-06 20:41:53 +0100

    implement sitemap crawler

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://knowledge-base.vercel.app',
+  url: 'https://knowledge.moritz-grimm.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -45,6 +45,9 @@ const config: Config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        sitemap: {
+            changefreq: "weekly",
+        }
       } satisfies Preset.Options,
     ],
   ],
