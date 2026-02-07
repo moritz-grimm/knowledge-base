2dc1147f1c77128aa90acbf3e8ad12a38329b216
moritz-grimm <molumo@icloud.com>
2026-02-07 01:24:14 +0100

    format docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,141 +1,141 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Knowledge Base',
-  favicon: 'img/favicon.ico',
+    title: 'Knowledge Base',
+    favicon: 'img/favicon.ico',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
-
-  // Set the production url of your site here
-  url: 'https://knowledge.moritz-grimm.dev',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'moritz-grimm', // Usually your GitHub org/user name.
-  projectName: 'knowledge-base', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'de'],
-  },
-
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-        },
-        blog: false,
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-        sitemap: {
-            changefreq: "weekly",
-        }
-      } satisfies Preset.Options,
-    ],
-  ],
-
-  plugins: [
-    [
-        'vercel-analytics',
-        {
-            debug: false
-        }
-    ],
-    [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
-      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
-      ({
-        hashed: true,
-        language: ["en", "de"],
-        indexBlog: false,
-        indexPages: true,
-        removeDefaultStopWordFilter: ["en", "de"],
-        removeDefaultStemmer: true,
-        fuzzyMatchingDistance: 2
-      }),
-    ],
-  ],
-
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    colorMode: {
-      respectPrefersColorScheme: true,
+    // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+    future: {
+        v4: true, // Improve compatibility with the upcoming Docusaurus v4
     },
-    navbar: {
-      title: 'Knowledge Base',
-      logo: {
-        alt: 'My GitHub Profile Picture',
-        src: 'img/gh-profile-picture.png',
-      },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'docs',
-          position: 'left',
-          label: 'Knowledge',
-        },
-        {
-            type: "localeDropdown",
-            position: "right"
-        },
-        {
-            type: "search",
-            position: "right"
-        },
-        {
-          href: 'https://github.com/moritz-grimm/knowledge-base',
-          position: 'right',
-          className: 'header-github-link',
-        },
-      ],
+
+    // Set the production url of your site here
+    url: 'https://knowledge.moritz-grimm.dev',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'moritz-grimm', // Usually your GitHub org/user name.
+    projectName: 'knowledge-base', // Usually your repo name.
+
+    onBrokenLinks: 'throw',
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en', 'de'],
     },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Links',
-          items: [
+
+    presets: [
+        [
+            'classic',
             {
-              label: 'GitHub',
-              href: 'https://github.com/moritz-grimm',
-            },
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                },
+                blog: false,
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+                sitemap: {
+                    changefreq: "weekly",
+                }
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    plugins: [
+        [
+            'vercel-analytics',
             {
-                label: "NPM",
-                href: "https://www.npmjs.com/~moritz-grimm"
-            },
-            {
-                label: "This Repo",
-                href: "https://github.com/moritz-grimm/knowledge-base"
-            },
-          ],
+                debug: false
+            }
+        ],
+        [
+            require.resolve("@easyops-cn/docusaurus-search-local"),
+            /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+            ({
+                hashed: true,
+                language: ["en", "de"],
+                indexBlog: false,
+                indexPages: true,
+                removeDefaultStopWordFilter: ["en", "de"],
+                removeDefaultStemmer: true,
+                fuzzyMatchingDistance: 2
+            }),
+        ],
+    ],
+
+    themeConfig: {
+        // Replace with your project's social card
+        image: 'img/docusaurus-social-card.jpg',
+        colorMode: {
+            respectPrefersColorScheme: true,
         },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Knowledge Base, Built with Docusaurus by Moritz.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+        navbar: {
+            title: 'Knowledge Base',
+            logo: {
+                alt: 'My GitHub Profile Picture',
+                src: 'img/gh-profile-picture.png',
+            },
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'docs',
+                    position: 'left',
+                    label: 'Knowledge',
+                },
+                {
+                    type: "localeDropdown",
+                    position: "right"
+                },
+                {
+                    type: "search",
+                    position: "right"
+                },
+                {
+                    href: 'https://github.com/moritz-grimm/knowledge-base',
+                    position: 'right',
+                    className: 'header-github-link',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Links',
+                    items: [
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/moritz-grimm',
+                        },
+                        {
+                            label: "NPM",
+                            href: "https://www.npmjs.com/~moritz-grimm"
+                        },
+                        {
+                            label: "This Repo",
+                            href: "https://github.com/moritz-grimm/knowledge-base"
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Knowledge Base, Built with Docusaurus by Moritz.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
 };
 
 export default config;
