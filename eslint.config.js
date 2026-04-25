bff60ba062fdf5d28b890a7645cd4f249fb3d315
moritz-grimm <molumo@icloud.com>
2026-04-25 15:47:23 +0200

    update eslint-config to 2.2.0; add linting options to tsconfig;

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import eslintConfig from "@moritz-grimm/eslint-config";
+import { globalIgnores } from "eslint/config";
 
 export default [
     ...eslintConfig,
+    globalIgnores([".docusaurus/"]),
 ];
